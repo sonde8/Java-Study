e184b109a429b8e3b15312e706d798999d70c9ca
sonde8 <sonde8@naver.com>
2025-08-25 11:22:55 +0900

    배열 문제 풀이

--- Pro01/src/ch02/quiz/Quiz_배열12번.java
+++ Pro01/src/ch02/quiz/Quiz_배열12번.java
@@ -1,10 +1,23 @@
 package ch02.quiz;
 
+import java.util.Scanner;
+
 public class Quiz_배열12번 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Scanner sc=new Scanner(System.in);
+		int[] numbers = new int[5];
+		
+		for(int i=0;i<numbers.length;i++) {
+			System.out.println("수를 입력하세요");
+			
+		}
+		
+		
+		
+		sc.close();
 	}
 
 }
